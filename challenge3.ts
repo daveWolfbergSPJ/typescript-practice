93ee523524f21158d8f8e6a70f13fb89d24b9ef6
Dave Wolfberg <davewolfberg@Daves-MacBook-Air.local>
2022-07-15 09:00:17 -0700

    challenge3 fixed

--- challenge3.ts
+++ challenge3.ts
@@ -2,10 +2,15 @@ type FilterFunction<T> = (data: T[keyof T]) => boolean;
 type Filters<T> = Record<keyof T, FilterFunction<T>[]>;
 type MapFunction<T> = (data: T[keyof T]) => T[keyof T];
 type Maps<T> = Record<keyof T, MapFunction<T>[]>;
+type ProcessedEvent<T> = {
+    eventName: keyof T,
+    data: T[keyof T]
+}
 
 class EventProcessor<T extends {}> {
   private filters: Filters<T> = <Filters<T>>{};
   private maps: Maps<T> = <Maps<T>>{};
+  private processed: ProcessedEvent<T>[] = [];
 
   handleEvent<K extends keyof T>(eventName: K, data: T[K]): void {
     let allowEvent = true;
@@ -17,31 +22,37 @@ class EventProcessor<T extends {}> {
     }
     if (allowEvent) {
       let mappedData = { ...data };
-      for (const map of this.map[eventName] ?? []) {
-        mappedData = map(map)
+      for (const map of this.maps[eventName] ?? []) {
+        mappedData = <T[K]> map(mappedData)
         }
+        this.processed.push({
+            eventName,
+            data: mappedData,
+        })
       } 
     }
-  }
+    
+    addFilter<K extends keyof T>(
+        eventName: K,
+        filter: (data: any) => boolean
+        ): void {
+            this.filters[eventName] ||= [];
+            this.filters[eventName].push(filter);
+        }
+        
+        addMap<K extends keyof T>(eventName: K, map: (data: any) => T[K]): void {
+            this.maps[eventName] ||= [];
+            this.maps[eventName].push(map);
+        }
+        
+        getProcessedEvents() {
+            return this.processed;
+        }
+    }
 
-  addFilter<K extends keyof T>(
-    eventName: K,
-    filter: (data: T[K]) => boolean
-  ): void {
-    this.filters[<string>eventName] ||= [];
-    this.filters[<string>eventName].push(filter);
-  }
-
-  addMap<K extends keyof T>(eventName: K, map: (data: T[K]) => T[K]): void {
-    this.maps[<string>eventName] ||= [];
-    this.maps[<string>eventName].push(map);
-  }
-
-  getProcessedEvents() {}
-}
 
 interface EventMap {
-  login: { user?: string; name?: string; hasSession?: boolean };
+  login: { user?: string | null; name?: string; hasSession?: boolean };
   logout: { user?: string };
 }
 
